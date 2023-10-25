aa1f690f13d5838393565ecd48b37b66c7db181f
Nahuel Brugnoni <nahubrugnoni@gmail.com>
2023-10-25 13:48:15 -0300

    Paginación completa del Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import NavBar from "./components/NavBar/NavBar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SonidoEnVivo from "./pages/SonidoEnVivo";
+import Microfono from "./pages/Microfono";
+import Backline from "./pages/Backline";
+import Iluminación from "./pages/Iluminacion";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <>
       <NavBar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/SonidoEnVivo" element={<SonidoEnVivo />}></Route>
+          <Route path="/Microfono" element={<Microfono />}></Route>
+          <Route path="/Backline" element={<Backline />}></Route>
+          <Route path="/Iluminacion" element={<Iluminación />}></Route>
+          <Route path="/Contact" element={<Contact />}></Route>
+        </Routes>
+      </div>
     </>
   );
 }
